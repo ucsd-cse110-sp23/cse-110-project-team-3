8b8ed6bec7bdd85386c8ca566603139970ec613e
Latortuga13 <56976660+Latortuga13@users.noreply.github.com>
2023-05-07 19:29:56 -0700

    Made UI for prompt history
    made a back and prompt history button

--- src/MainPage.java
+++ src/MainPage.java
@@ -5,10 +5,12 @@ import javax.sound.sampled.*;
 import javax.swing.*;
 
 class Header extends JPanel {
-
+    private JButton promptHistoryButton;
     Color backgroundColor = new Color(240, 248, 255);
   
     Header() {
+      promptHistoryButton = new JButton("prompt history");
+      this.add(promptHistoryButton);
       this.setPreferredSize(new Dimension(400, 60)); // Size of the header
       this.setBackground(backgroundColor);
       JLabel titleText = new JLabel("Saylt Assistant"); // Text of the header
@@ -16,6 +18,10 @@ class Header extends JPanel {
       titleText.setFont(new Font("Sans-serif", Font.BOLD, 20));
       titleText.setHorizontalAlignment(JLabel.CENTER); // Align the text to the center
       this.add(titleText); // Add the text to the header
+    }
+
+    public JButton getpromptHistoryButton(){
+        return promptHistoryButton;
     }
 }
 
@@ -98,13 +104,53 @@ class Footer extends JPanel {
     }
 }
 
+class promptHeader extends JPanel{
+    Color backgroundColor = new Color(240, 248, 255);
+    JButton backButton;
+
+    promptHeader(){
+        this.setPreferredSize(new Dimension(400, 60)); // Size of the header
+        this.setBackground(backgroundColor);
+        backButton = new JButton("back");
+        this.add(backButton);
+        JLabel titleText = new JLabel("Prompt History");
+        titleText.setPreferredSize(new Dimension(200, 60));
+        titleText.setFont(new Font("Sans-serif", Font.BOLD, 20));
+        titleText.setHorizontalAlignment(JLabel.CENTER); // Align the text to the center
+        this.add(titleText); // Add the text to the header
+    }
+
+    public JButton getbackButton(){
+        return backButton;
+    }
+}
+
+class promptBody extends JPanel{
+    JLabel tLabel;
+
+    promptBody(){
+        this.tLabel = new JLabel();
+
+        this.setPreferredSize(new Dimension(400, 60));
+        tLabel.setText("Past user inputted questions and answers\n");
+        tLabel.setHorizontalAlignment(SwingConstants.CENTER);
+        tLabel.setFont(new Font("Sans-serif", Font.ITALIC, 20));
+
+        this.add(tLabel);
+    }
+}
+
 class MainPage extends JFrame {
     private Header header;
     private Footer footer;
     private resultUI resultUI;
+    private promptBody promptBody;
+    private promptHeader promptHeader;
 
     private JButton pauseButton;
     private JButton newQuestionButton;
+    private JButton prompthistoryButton;
+    private JButton backButton;
 
     MainPage(){
         this.setSize(600, 600);
@@ -119,7 +165,60 @@ class MainPage extends JFrame {
         this.add(footer, BorderLayout.SOUTH); // Add footer on bottom of the screen
         this.add(resultUI, BorderLayout.CENTER); // adds question and response to center of screen
         
+        prompthistoryButton = header.getpromptHistoryButton();
         pauseButton = footer.getPauseButton();
         newQuestionButton = footer.getnewQuestionButton();
+
+        buttonLogic();
+    }
+
+    public void openPromptPage(){
+        this.setSize(600, 600);
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); // Close on exit
+        this.setVisible(true); // Make visible
+
+        promptBody = new promptBody();
+        promptHeader = new promptHeader();
+
+        this.remove(header);
+        this.remove(footer);
+        this.remove(resultUI);
+        this.add(promptHeader, BorderLayout.NORTH);
+        this.add(promptBody, BorderLayout.CENTER);
+
+        backButton = promptHeader.getbackButton();
+    }
+    
+    public void openMainPage(){
+        this.setSize(600, 600);
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); // Close on exit
+        this.setVisible(true); // Make visible
+
+        this.remove(promptHeader);
+        this.remove(promptBody);
+        this.add(header, BorderLayout.NORTH); // Add title bar on top of the screen
+        this.add(footer, BorderLayout.SOUTH); // Add footer on bottom of the screen
+        this.add(resultUI, BorderLayout.CENTER); // adds question and response to center of screen
+        
+        prompthistoryButton = header.getpromptHistoryButton();
+        pauseButton = footer.getPauseButton();
+        newQuestionButton = footer.getnewQuestionButton();
+    }
+
+    public void buttonLogic(){
+        prompthistoryButton.addMouseListener(
+            new MouseAdapter() {
+                public void mousePressed(MouseEvent e){
+                    openPromptPage();
+                    backButton.addMouseListener(
+                        new MouseAdapter() {
+                            public void mousePressed(MouseEvent e){
+                                openMainPage();
+                            }
+                        }
+                    );
+                }
+            }
+        );
     }
 }
