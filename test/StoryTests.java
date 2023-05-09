2c3a5c8a1e30f2b30231d8bf8c66faeb01a41df5
Kenneth Chung <pg7408@gmail.com>
2023-05-08 20:06:45 -0700

    src

--- test/StoryTests.java
+++ test/StoryTests.java
@@ -10,25 +10,25 @@ import java.util.Scanner;
 
 public class StoryTests {
 
-    // // story 1 test
-    // @Test
-    // void testStoryAskQuestion() {
+    // story 1 test
+    @Test
+    void testStoryAskQuestion() {
 
-    //     // mock process recording into text prompt
-    //     IWhisper iWhisper = new MockWhisper();
-    //     String mockRecordingFilePath = "";
-    //     String mockPrompt = "";
-    //     try {
-    //         mockPrompt = iWhisper.generate(mockRecordingFilePath);
-    //     } catch (Exception e) {
-    //         //
-    //     }
+        // mock process recording into text prompt
+        IWhisper iWhisper = new MockWhisper();
+        String mockRecordingFilePath = "";
+        String mockPrompt = "";
+        try {
+            mockPrompt = iWhisper.generate(mockRecordingFilePath);
+        } catch (Exception e) {
+            //
+        }
         
-    //     // mock process text prompt into text answer
-    //     IGPT mockGpt = new MockGPT();
-    //     String mockAnswer = mockGpt.generate(mockPrompt);
-    //     assertEquals("Mock Prompt: " + mockPrompt, mockAnswer);
+        // mock process text prompt into text answer
+        IGPT mockGpt = new MockGPT();
+        String mockAnswer = mockGpt.generate(mockPrompt);
+        assertEquals("Mock Prompt: " + mockPrompt, mockAnswer);
         
-    // }
+    }
 
 }
