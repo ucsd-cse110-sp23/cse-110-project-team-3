cca3ac8817aec5c41eea8f02793463ae8a79369c
Kenneth Chung <pg7408@gmail.com>
2023-05-27 17:43:58 -0700

    create account now works, but ip address of user needs to be manually added to database; fix next time lol

--- API/QuickStart/Credentials.java
+++ API/QuickStart/Credentials.java
@@ -11,6 +11,8 @@ import com.mongodb.client.MongoClients;
 import com.mongodb.client.MongoCollection;
 import com.mongodb.client.MongoDatabase;
 
+import org.bson.types.ObjectId;
+
 public class Credentials {
     
     private final String uri = "mongodb+srv://k2chung:suqNIH8XW2du0NId@sayit.gzgbzwy.mongodb.net/?retryWrites=true&w=majority";
@@ -21,29 +23,30 @@ public class Credentials {
 
     public void createAccount(String username, String password) {
 
-        // check if account with username already exists
         try (MongoClient mongoClient = MongoClients.create(uri)) {
-
+            
+            // check if account with username already exists
             MongoDatabase database = mongoClient.getDatabase("userdata");
             MongoCollection<Document> collection = database.getCollection("accounts");
 
             Document doc = collection.find(eq("username", username)).first();
             if (doc != null) {
-                System.out.println("Username is already taken");
+                throw new Exception("Account with username already exists");
             } else {
                 System.out.println("Username is free!");
             }
 
+            // add account with username and password
+            Document newAccount = new Document("_id", new ObjectId());
+            newAccount.append("username", username);
+            newAccount.append("password", password);
+            
+            collection.insertOne(newAccount);
+            
+        } catch (Exception e) {
+            System.err.println(e);
         }
 
-        // add account with username and password
-        //
-
-    }
-
-    public static void main(String args[]) {
-        Credentials c = new Credentials();
-        c.createAccount("k4chung", "Rotary552");
     }
 
 }
