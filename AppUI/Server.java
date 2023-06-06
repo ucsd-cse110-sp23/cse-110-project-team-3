e48041fe4f8415cf6d335de3134665dab3fbf7dd
Latortuga13 <56976660+Latortuga13@users.noreply.github.com>
2023-06-06 10:48:18 -0700

    fixing bugs
    above

--- AppUI/Server.java
+++ AppUI/Server.java
@@ -12,7 +12,7 @@ import java.util.concurrent.*;
 public class Server {
     private static final int SERVER_PORT = 8101;
     private static final String SERVER_HOSTNAME = "localhost";
-    Mediator m = new Mediator();
+    private final Mediator m = new Mediator();
     
     Server() throws IOException{
         ThreadPoolExecutor threadPoolExecutor = (ThreadPoolExecutor) Executors.newFixedThreadPool(10);
